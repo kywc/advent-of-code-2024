9f4b8416cda9e42096dbf0b168222a2c194cc2e5
kywc <wolvesys@gmail.com>
2024-12-05 21:25:00 -0500

    4

--- 4/main.zig
+++ 4/main.zig
@@ -2,19 +2,21 @@ var text: [140][140]u8 = undefined;
 var fba = std.heap.FixedBufferAllocator.init(&text);
 const alloc = fba.allocator();
 
-const Direction = enum(i2) {
-    fwd = 1,
-    rev = -1,
-    nul = 0,
-};
-const Progress = enum(u2) {
-    X = 0,
-    M = 1,
-    A = 2,
-    S = 3,
+//  const Direction = enum(i2) {
+//     fwd = 1,
+//     rev = -1,
+//     nul = 0,
+// };
+const Progress = enum(i3) {
+    Mb = -3,
+    Ab = -2,
+    Sb = -1,
+    Z = 0,
+    Xf = 1,
+    Mf = 2,
+    Af = 3,
 };
 const ReadPtr = struct {
-    dir: Direction,
     stat: Progress,
-    ptr: *u8,
+    ptr: *[_]u8,
 };
